00bdc43ac155d460973d866c754732636b5146cb
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2024-05-14 13:44:34 -0400

    optional semi colons

--- test_parser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-npm run build
-
-FILENAME=$1
-
-node "${SCRIPT_DIR}/dist/index.js" "${SCRIPT_DIR}/tests/${FILENAME}" -t parser
