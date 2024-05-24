ee018712335720ce31cff101b253f73e91d98a96
Faisal <faisaladamj@gmail.com>
2024-05-23 20:15:04 -0400

    added express + basic webpage with WebspeechAPI integration (first iteration)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 
 import { handleChatModel } from './switchingLogic.js';
+import express from 'express';
 
 
+//handleChatModel();
 
+const app = express();
+const PORT = 7000;
 
-handleChatModel();
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
 
-
-
-
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
