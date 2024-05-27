3b29f6917b14bf82dd1ac85256aed7b9a60fcca0
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2024-05-27 14:31:25 -0400

    installed and setup jest to work with ecmascript modules

---
+++ tests/test_cases.ts
@@ -0,0 +1,6 @@
+export const identifier_declaration_test = "number one = 1";
+export const keyword_identifier_test = 'string number = "number"';
+export const newline_seperated_statements_test =
+  'string output = "Hello World!"\nprint(output)';
+export const comma_seperated_statements_test =
+  'string output = "Hello World!";print(output)';
