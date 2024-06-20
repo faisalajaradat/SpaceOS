8c84dc727aa0d6a7e12c142a8ee5c9d0ece401cb
Samuel Faubert <samuel.faubert@mail.mcgill.ca>
2024-06-20 16:37:17 -0400

    started saving object definitions

--- SpatialComputingEngine/src/FrontendObjects.ts
+++ SpatialComputingEngine/src/FrontendObjects.ts
@@ -1,34 +1,70 @@
-import { Schema, Repository, Entity } from "redis-om";
-import { connect, client } from "./SpatialComputingEngine.js";
+import { Entity, Schema, SchemaDefinition } from "redis-om";
 
-export interface SpatialEntity extends Entity {
+export abstract class SpatialType implements Entity {
+  [index: string]: string | boolean;
   locality: string;
+
+  constructor(locality: string) {
+    this.locality = locality;
+  }
 }
 
-const entitySchema = new Schema(
-  "SpatialEntity",
-  {
-    locality: { type: "string" },
-  },
-  { dataStructure: "JSON" },
-);
+export const spatialTypeSchemaDef: SchemaDefinition = {
+  locality: { type: "string" },
+};
 
-export async function saveEntity(
-  entity: SpatialEntity,
-): Promise<SpatialEntity> {
-  await connect();
+export abstract class SpatialObject extends SpatialType {
+  isControlled: boolean;
 
-  const repo = new Repository(entitySchema, client);
-
-  const id: SpatialEntity = <SpatialEntity>await repo.save(entity);
-
-  return id;
+  constructor(locality: string, isControlled: boolean) {
+    super(locality);
+    this.isControlled = isControlled;
+  }
 }
 
-export async function fetchEntity(id: string): Promise<SpatialEntity> {
-  await connect();
+export const spatialObjectSchemaDef: SchemaDefinition = {
+  ...spatialTypeSchemaDef,
+  isControlled: { type: "boolean" },
+};
 
-  const repo = new Repository(entitySchema, client);
+export abstract class DynamicEntity extends SpatialObject {
+  motion: string;
 
-  return <SpatialEntity>await repo.fetch(id);
+  constructor(locality: string, isControlled: boolean, motion: string) {
+    super(locality, isControlled);
+    this.motion = motion;
+  }
+}
+
+export const dynamicEntitySchemaDef: SchemaDefinition = {
+  ...spatialObjectSchemaDef,
+  motion: { type: "string" },
+};
+
+export class OpenSpace extends SpatialObject {}
+
+export class EnclosedSpace extends SpatialObject {}
+
+export class StaticEntity extends SpatialObject {}
+
+export class AnimateEntity extends DynamicEntity {}
+
+export class SmartEntity extends DynamicEntity {}
+
+export class Path extends SpatialType {
+  constructor(locality: string) {
+    super(locality);
+  }
+}
+
+export class AirPath extends Path {
+  constructor() {
+    super("physical");
+  }
+}
+
+export class LandPath extends Path {
+  constructor() {
+    super("physical");
+  }
 }
--- SpatialComputingEngine/src/SpatialComputingEngine.ts
+++ SpatialComputingEngine/src/SpatialComputingEngine.ts
@@ -1,9 +1,50 @@
 import "dotenv/config";
-import { Client } from "redis-om";
-import { SpatialEntity, fetchEntity, saveEntity } from "./FrontendObjects.js";
+import { Client, EntityId, Repository, Schema } from "redis-om";
+import {
+  DynamicEntity,
+  SpatialObject,
+  SpatialType,
+  dynamicEntitySchemaDef,
+  spatialObjectSchemaDef,
+  spatialTypeSchemaDef,
+} from "./FrontendObjects.js";
 
-export const client = new Client();
+const client = new Client();
 
-export async function connect() {
+async function connect() {
   if (!client.isOpen()) await client.open(process.env.REDIS_URL);
 }
+
+function getRepo(data: SpatialType): Repository {
+  const schemaDef =
+    data instanceof DynamicEntity
+      ? dynamicEntitySchemaDef
+      : data instanceof SpatialObject
+        ? spatialObjectSchemaDef
+        : spatialTypeSchemaDef;
+  return new Repository(new Schema(data.constructor.name, schemaDef), client);
+}
+
+export async function saveData(data: SpatialType): Promise<string> {
+  await connect();
+
+  const repo = getRepo(data);
+
+  const enrichedEntity = await repo.save(data);
+
+  return enrichedEntity[EntityId];
+}
+
+/*const entity: SmartEntity = new SmartEntity("virtual", true, "mobile");
+
+console.log(entity);
+
+console.log(await saveData(entity));*/
+await connect();
+
+console.log(
+  await new Repository(
+    new Schema("SmartEntity", dynamicEntitySchemaDef),
+    client,
+  ).fetch("01J0VN5NSB8QJEV1V0T4HP1NJ8"),
+);
